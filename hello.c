0dd4b9e8fe236dfbe21b099923108a8d2bba62f9
Rathod Raja <rajarathod214@yahoo.com>
2018-03-21 18:36:37 +0530

    for merging

--- hello.c
+++ hello.c
@@ -6,6 +6,9 @@ int main()
 	printf("Hello world !!!!\n");
 	printf("Hello world !!!!\n");
 	printf("Hello world !!!!\n");
+	printf("Hello world !!!!\n");
+	printf("Hello world !!!!\n");
+	printf("Hello world !!!!\n");
 
 return 0;
 }
